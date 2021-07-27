d647163d87cca2aa885e1e86cba8a3712b4a13ec
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-07-27 11:38:44 -0300

    mainscreen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'mainscreen',
     pathMatch: 'full'
+  },
+  {
+    path: 'mainscreen',
+    loadChildren: () => import('./mainscreen/mainscreen.module').then(m => m.MainscreenPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
 ];
 
